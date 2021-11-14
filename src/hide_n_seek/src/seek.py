5656a189c9115a8a6ff2359ff49e41ac7ff97e8a
Jai <jksmithnyc@gmail.com>
2021-11-13 19:34:04 -0500

    update seek script with nav

--- src/hide_n_seek/src/seek.py
+++ src/hide_n_seek/src/seek.py
@@ -1,15 +1,25 @@
 from threading import Thread
+import math
 import rospy
+import sys
 
-from motion import Motion
-from mapper import Mapper
+sys_paths = ['../']
+for p in sys_paths:
+  p = os.path.abspath(p)
+  if p not in sys.path:
+      sys.path.append(p)
+
+from nodes.motion_node import Motion
+from nodes.mapper_node import Mapper
+from nodes.navigation_node import Navigation
 
 rospy.init_node('seek')
 
 rospy.sleep(5)
 
-motion = Motion()
+motion = Motion(linear_vel=0.1, angular_vel=math.pi / 8)
 mapper = Mapper()
+navigation = Navigation()
 
 def shutdown():
   motion.stop()
@@ -19,3 +29,4 @@ rospy.on_shutdown(shutdown)
 
 Thread(target=mapper.spin).start()
 Thread(target=motion.run).start()
+Thread(target=navigation.spin).start()
