05e3cd5439a4a841b35491f0fc99bbda36ac3987
Ari Chadda <59665721+arichadda@users.noreply.github.com>
2021-11-19 17:34:47 -0500

    Update object_detection.py

--- src/hide_n_seek/src/object_detection.py
+++ src/hide_n_seek/src/object_detection.py
@@ -15,7 +15,7 @@ from matplotlib import patches, patheffects
 from paramiko import SSHClient
 from scp import SCPClient
 
-
+# add other directory dependencies
 sys_paths = ['../']
 for p in sys_paths:
     p = os.path.abspath(p)
@@ -27,12 +27,12 @@ from yolov3_pytorch.yolov3 import *
 from yolov3_pytorch.yolov3_tiny import *
 
 FREQUENCY = 10
-IMG_WIDTH = 640
+IMG_WIDTH = 640 # from ROSbot camera launch files
 IMG_HEIGHT = 480
-FOV = 60
+FOV = 60 # camera field of view
 
 class ObjectDetection:
-    def __init__(self):
+    def __init__(self): # adding MS COCO classes
         self.class_names = ['person', 'bicycle', 'car', 'motorbike', 'aeroplane', 'bus', \
         'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', \
         'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', \
@@ -47,34 +47,34 @@ class ObjectDetection:
         'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
 
         self.sz = 416
-
+        # subscribe to image on robot
         self._img_sub = rospy.Subscriber('camera/rgb/image_raw/compressed', CompressedImage, callback=self._image_callback, queue_size=1)
 
         self.rel_angle = None
         self.timestamp_secs = None
         self.timestamp_nsecs = None
 
-        self.model = Yolov3Tiny(num_classes=len(self.class_names))
-        self.model.load_state_dict(torch.load('../yolov3_pytorch/yolov3_tiny_coco_01.h5'))
+        self.model = Yolov3Tiny(num_classes=len(self.class_names)) # implementation from https://github.com/holli/yolov3_pytorch
+        self.model.load_state_dict(torch.load('../yolov3_pytorch/yolov3_tiny_coco_01.h5')) # weights based on https://pjreddie.com/darknet/yolo/ 
 
     # http://wiki.ros.org/rospy_tutorials/Tutorials/WritingImagePublisherSubscriber
     # https://github.com/holli/yolov3_pytorch
     def _image_callback(self, msg):
-        np_arr = np.fromstring(msg.data, np.uint8)
+        np_arr = np.fromstring(msg.data, np.uint8) # convert compressed image to rgb image
         image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
         img_org = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
         img_org = Image.fromarray(img_org)
         img_resized = img_org.resize((self.sz, self.sz))
-        img_torch = image2torch(img_resized)
+        img_torch = image2torch(img_resized) # conver to tensor
 
-        all_boxes = self.model.predict_img(img_torch, conf_thresh=0.2)[0]
+        all_boxes = self.model.predict_img(img_torch, conf_thresh=0.2)[0] # get preds
         boxes_found = nms(all_boxes, 0.3)
         b = np.array(boxes_found)
         if len(b) > 0:
             classes = b[:, -1].astype(int)
             boxes = b[:, 0:4]
 
-            boxes[:, 0] *= IMG_WIDTH
+            boxes[:, 0] *= IMG_WIDTH # draw bounding boxes dims
             boxes[:, 2] *= IMG_WIDTH
             boxes[:, 1] *= IMG_HEIGHT
             boxes[:, 3] *= IMG_HEIGHT
@@ -84,17 +84,17 @@ class ObjectDetection:
 
                 if b[0] == 0:
                     break
-                if self.class_names[classes[i]] == 'clock':
+                if self.class_names[classes[i]] == 'clock': # object of interest
                     self.timestamp_secs = msg.header.stamp.secs
                     self.timestamp_nsecs = msg.header.stamp.nsecs
                     x, y = b[0], b[1]
                     w, h = b[2], b[3]
 
                     bb_bottom = (x + w/2, y)
-                    rel_angle = np.radians((FOV/(IMG_WIDTH) * bb_bottom[0] - FOV/2) * -1)
+                    rel_angle = np.radians((FOV/(IMG_WIDTH) * bb_bottom[0] - FOV/2) * -1) # angle adjustment computation
                     self.rel_angle = rel_angle
 
-            plot_img_detections(img_resized, boxes_found, figsize=(8,8), class_names=self.class_names)
+            plot_img_detections(img_resized, boxes_found, figsize=(8,8), class_names=self.class_names) # plot to viz
 
     # https://stackoverflow.com/questions/43577248/scp-in-python-by-using-password
     def ssh_scp_files(self, ssh_host, ssh_user, ssh_password, ssh_port, source_volume, destination_volume):
@@ -103,19 +103,19 @@ class ObjectDetection:
         ssh.connect(ssh_host, username=ssh_user, password=ssh_password, look_for_keys=False)
 
         with SCPClient(ssh.get_transport()) as scp:
-            scp.put(source_volume, recursive=True, remote_path=destination_volume)
+            scp.put(source_volume, recursive=True, remote_path=destination_volume) # copy over via scp b/c publishing to robot restricted
 
     def detect(self):
         rate = rospy.Rate(FREQUENCY)
         while not rospy.is_shutdown():
             if self.rel_angle:
                 with open('./object_angle.txt', 'w') as f:
-                    f.write(str(self.rel_angle) + ' ' + str(self.timestamp_secs) + ' ' + str(self.timestamp_nsecs))
+                    f.write(str(self.rel_angle) + ' ' + str(self.timestamp_secs) + ' ' + str(self.timestamp_nsecs)) # write sending file and send
                 self.ssh_scp_files(ssh_host='192.168.0.1', ssh_user='husarion', ssh_password='husarion', ssh_port='11311', source_volume='./object_angle.txt', destination_volume='/home/husarion/husarion_ws/src/hide-n-seek/src/hide_n_seek/nodes/object_angle.txt')
                 rate.sleep()
 
 if __name__ == "__main__":
-    rospy.init_node('object_detection')
+    rospy.init_node('object_detection') # create node
     object_detection = ObjectDetection()
     rospy.sleep(2)
     object_detection.detect()
